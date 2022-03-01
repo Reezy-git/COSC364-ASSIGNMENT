8b1a56a65d6abacec699c168ef387bad2a33c0f1
r00t <aerodynamicrat@gmail.com>
2022-03-01 16:33:42 +1300

    We will use this server implementation as starting point for our router

--- UDP_SERVER_CLASS.py
+++ UDP_SERVER_CLASS.py
@@ -4,16 +4,17 @@ import socket
 
 class Server:
     def __init__(self, address, port):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket to receive
         host = address, port
-        self.s.bind(host)
+        self.receiver.bind(host)
+        self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket to send
 
     def fileno(self):  # required for select
-        return self.s.fileno()
+        return self.receiver.fileno()
 
     def on_read(self):  # the method for receiving a message
         """todo: process message into something useful i.e. receive new address data"""
-        message = self.s.recv(1024).decode('utf-8')
+        message = self.receiver.recv(1024).decode('utf-8')
         print(message)
 
 
