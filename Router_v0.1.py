779676d4fa4c022555235e9508af4495704c6972
r00t <aerodynamicrat@gmail.com>
2022-03-07 11:08:00 +1300

    Started implementing routing aspect.

--- Router_v0.1.py
+++ Router_v0.1.py
@@ -11,41 +11,65 @@ import sys
 
 
 class Server:
-    def __init__(self, address, port):
+    def __init__(self, address, port, owner):
         self.receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket to receive
-        host = address, port
+        host = 'localhost', port
         self.receiver.bind(host)
-        self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket to send
+        self.owner  # the router which owns this port
 
     def fileno(self):  # required for select
         return self.receiver.fileno()
 
     def on_read(self):  # the method for receiving a message
-        """todo: process message into something useful i.e. receive new address data"""
+        """todo: send info to my router (message + my port)"""
         message = self.receiver.recv(1024).decode('utf-8')
-        print(message)
+        print('Send to ', self.owner)
 
 
-class Destination:
-    """
-    Route description database for destination on network
-    todo: complete init, write distance algorithm
-    """
+class Router:
+    def __init__(self, router_id, outputs):
+        self.router_id = router_id
+        self.outputs = outputs
+        self.f_table = {1: (5000, 8), 2: (5001, 2)}  # forwarding table
+        self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket to send
 
-    def __init__(self, address, router, interface, metric, time):
-        self.address = address  # the address/ID of the destination.
-        self.router = router  # address of next hop to reach destination
-        self.metric = metric  # integer indicating distance
-        self.time = time  # time since last communication, needs to count to 180seconds then timeout as unreachable
+    def recv_msg(self, msg, port):
+        msg_dst = msg[0:8]
+        if msg_dst == self.router_id:
+            print('Message received at router', self.router_id)
+        else:
+            if self.f_table.__contains__(msg_dst):
+                self.sender.sendto(msg, self.f_table[msg_dst][0])
 
+    def update_f_table(self, new_info):
+        """todo: write me"""
+
+
+# router_info ={'1': [[1112, 1116, 1117], [2221, 7777, 6666]],
+#               '2': [[2221, 2223], [1112, 2223]],
+#               '3': [[3332, 3334], [2223, 4443]],
+#               '4': [[4443, 4445, 4447], [7774, 3334, 5554]],
+#               '5': [[5554, 5556], [4445, 6665]],
+#               '6':[[6661, 6665], [1116, 5556]],
+#               '7': [[7771, 7774], [1117, 4447]]}
+
+test_routers = {'1': [[5000], [5001]],
+                '2': [[5001], [5000]]}
 
 def main():
     """I run the show around here!
-    todo: implement send/recv via udp and select"""
-    server = Server('localhost', 5010)
+    todo: see comment on line 66"""
+    servers = []
+    for router in test_routers:
+        router = Router(router, test_routers[router][1])
+        for port in test_routers[router][0]:
+            """create a server with below details """
+            Server('localhost', int(port), router)
+
+    print(len(servers))
 
     while True:
-        readers, _, _ = select.select([server], [], [])  # select takes 3 lists as input we only need first one
+        readers, _, _ = select.select([servers], [], [])  # select takes 3 lists as input we only need first one
         for reader in readers:
             reader.on_read()
 
