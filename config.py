5b9d1b40a3f6ef70e5b08dfb40b24bfde944f91e
Lenovo <ccq10@uclive.ac.nz>
2022-03-22 15:56:09 +1300

    Tweaked the config file use python3 config.py router_file

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import sys
 import re
 
+
 # input port instance router listen to
 # output how to reach rich neighbor
 # send data to neighbor poisoined reverse
@@ -13,18 +14,25 @@ class Main:
         self.router_id = router_id
         self.input_ports = input_ports
         self.outputs = outputs
-        self.routing_dic = {}
+        self.routing_dictionary = {}
+        # For loop to get information of output port in a dictionary template
         for neighbor_port, cost, neighbor_id in outputs:
-            self.routing_dic[neighbor_id] = (int(cost), int(neighbor_port))
+            self.routing_dictionary[neighbor_id] = (int(cost), int(neighbor_port))
+        #self.routing_table = [self.router_id, self.routing_dictionary]
+        #[1, {2: (1, 2221), 7: (8, 7771), 6: (5, 6661)}]
 
-    # print the dictionary for now.
+    # Human-readable routing table returned that can be printed in terminal
     def print_routing_table(self):
-        table_format = "RIPv2 Routing Table of: " + str(self.router_id) + "\n"
-        table_format += "="*40
-        table_format += "\n" + str(self.routing_dic)
+        table_format = "="*18
+        table_format += " RIPv2 Routing Table of " + str(self.router_id) + " "
+        table_format += "="*18 + "\n"
+        table_format += "Router Inputs: " + str(self.input_ports) + "\n"
+        table_format += f"{'Router Id':<15}{'Port':>6}{'Cost':>20}{'Next Hop':>21}"
+        table_format += "\n" + "="*62 + "\n"
+        for key, value in sorted(self.routing_dictionary.items()):
+            port, cost = value
+            table_format += "{:<12} {:>6}  {:>20} \n".format(key, port, cost)
         print(table_format)
-
-
 
 
 # Read the router config file
@@ -42,16 +50,17 @@ def read_router_file(filename):
     for router_content in router_contents:
         router_content = re.split(", | |\n", router_content)
         content_list.append(router_content)
-        #print(content_list)
+        # print(content_list)
 
     for contents in content_list:
         # Taking the router id
         if "router-id" in contents:
             try:
                 router_num = int(contents[1])
-                #print(router_num)
+                # print(router_num)
             except ValueError:
                 print("Invalid router id")
+                sys.exit()
 
         # Taking the input ports
         if "input-ports" in contents:
@@ -60,39 +69,50 @@ def read_router_file(filename):
                 input_list = list(map(int, input_list))
                 if len(input_list) == 0:
                     print("No input ports found")
-                else:
-                    print("Valid Inputs")
-                    #print(input_list)
+                    sys.exit()
             except ValueError:
-                print("Invalid input ports")
+                print("Invalid input ports/ No input ports found")
+                sys.ext()
 
         # Taking the output ports
         if "outputs" in contents:
             try:
                 output_nums = []
                 output_list = contents[1:]
-                #print(output_list)
+                # print(output_list)
                 for outputs in output_list:
                     split_output = outputs.split("-")
                     output_nums.append(split_output)
-                #print("hello", output_nums)
+                # print("hello", output_nums)
                 for output in output_nums:
                     output[0], output[1], output[2] = int(output[0]), int(output[1]), int(output[2])
-                    neighbor_port, cost, neighbor_id =  output[0], output[1], output[2]
+                    neighbor_port, cost, neighbor_id = output[0], output[1], output[2]
                 print(neighbor_port, cost, neighbor_id)
             except ValueError:
                 print("Invalid outputs")
+                sys.exit()
     return router_num, input_list, output_nums
 
 
 def main():
+
+    # Take the system argument 1 for txt file
+    try:
+        router_file = sys.argv[1]
+    except IndexError:
+        print("You did not specify a file")
+        sys.exit(1)
+
     # read config file
-    router_id, input_ports, outputs = read_router_file("router2.txt")
+    router_id, input_ports, outputs = read_router_file(router_file)
+
     # print(router_id, '0000')
     router = Main(router_id, input_ports, outputs)
-    #print(router.routing_dic)
-    router.print_routing_table()
 
+    # Print the routing table of the router
+    router.print_routing_table()
 
 if __name__ == "__main__":
     main()
+
+
