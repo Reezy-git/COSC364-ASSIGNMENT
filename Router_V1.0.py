44cf0684692a30fd8e485441538c9d8253235660
Reezy <aerodynamicrat@gmail.com>
2022-03-30 17:33:02 +1300

    Got my work from uni. Check recv_msg in Router_V1.0.py

--- Router_V1.0.py
+++ Router_V1.0.py
@@ -32,7 +32,7 @@ class Router:
     def __init__(self, router_id):
         self.router_id = router_id
         self.links = []
-        self.f_table = {1: (5000, 8), 2: (5001, 2)}  # forwarding table
+        self.f_table = {router_id: (0, 0)}  # forwarding table
         self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket to send
 
     def add_link(self, link):
@@ -43,8 +43,16 @@ class Router:
             if str(int(msg[:6], 2)) == self.router_id or str(int(msg[:6], 2)) == '0':
                 print(str(int(msg[:6], 2)))
                 msg_dst, typ, body = self.pkt_unravel(msg)
-                print('Message to ', msg_dst, 'received at router', self.router_id, 'Message type: ', typ)
-                print(body)
+                if typ == 0:
+                    print('Update f_table with', body)
+                if typ == 1:
+                    print('Router', self.router_id, 'received message', body)
+                if typ == 2:
+                    # print the f_table
+                    pass
+                if typ == 3:
+                    # maybe disable router?
+                    pass
             else:
                 msg_dst = str(int(msg[:6], 2))
                 print('Router', self.router_id, 'received message to forward to router', msg_dst)
@@ -53,41 +61,30 @@ class Router:
         except ValueError:
             print('Incorrect message format received at Router ID:', self.router_id)
 
-
-
-
     def update_f_table(self, new_info):
         """todo: write me"""
 
-    def pkt_build(self, dst, typ, body):
+    @staticmethod
+    def pkt_build(dst, typ, body):
         """Creates the correctly formatted packet to send between routers"""
-        bin_dst = bin(int(dst))[2:].zfill(6)  # first 6 digits represent destin>
-        bin_type = bin(int(typ))[2:].zfill(2)  # 2 digits for type = 4 possible>
-        bin_body = bin(int((json.dumps(body).encode('utf8')).hex(), 16))[2:]  #>
+        bin_dst = bin(int(dst))[2:].zfill(6)  # first 6 digits represent destin
+        bin_type = bin(int(typ))[2:].zfill(2)  # 2 digits for type = 4 possible types
+        bin_body = bin(int((json.dumps(body).encode('utf8')).hex(), 16))[2:]
         return bin_dst + bin_type + bin_body
 
-    def pkt_unravel(self, msg):
+    @staticmethod
+    def pkt_unravel(msg):
         dst = str(int(msg[:6], 2))
-        typ = str(int(msg[6:8], 2))
+        typ = int(msg[6:8], 2)
         body = json.loads(bytes.fromhex(hex(int(msg[8:], 2))[2:]))
         return dst, typ, body
 
 
-
-# router_info ={'1': [[1112, 1116, 1117], [2221, 7777, 6666]],
-#               '2': [[2221, 2223], [1112, 2223]],
-#               '3': [[3332, 3334], [2223, 4443]],
-#               '4': [[4443, 4445, 4447], [7774, 3334, 5554]],
-#               '5': [[5554, 5556], [4445, 6665]],
-#               '6':[[6661, 6665], [1116, 5556]],
-#               '7': [[7771, 7774], [1117, 4447]]}
-
 test_routers = {'1': [(5000, 5001, 8), (5011, 41241, 8)]}  # dictionary format {id: [(input, output, cost), (link2))]}
+
 
 def main():
     """I run the show around here!"""
-
-
     servers = []  # create a list to hold servers
     routers = []  # create a list
     i = 0
@@ -105,4 +102,3 @@ def main():
 
 
 main()
-
