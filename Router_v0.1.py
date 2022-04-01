da96ced4a42e34f785c3ead1907bf2961aa3a5c3
Lenovo <ccq10@uclive.ac.nz>
2022-04-01 20:48:04 +1300

    added string function

--- Router_v0.1.py
+++ Router_v0.1.py
@@ -1,6 +1,6 @@
 
 """A basic router for RIP implementation
-todo: Router: define f_table(forwarding table), write __str__ to print f_table, write recv_msg
+todo: Router: write __str__ to print f_table, write broadcasting function
 todo: Main: implement config reader and use to create server and router objects"""
 
 
@@ -8,6 +8,7 @@ import socket
 import select
 import threading  # Probably won't be used
 import sys
+import json
 import config
 
 
@@ -24,7 +25,6 @@ class Server:
 
     def on_read(self):  # the method for receiving a message
         message = self.receiver.recv(1024).decode('utf-8')
-        #print('Send to ', self.owner)
         self.owner.recv_msg(message, self.port)
 
 
@@ -32,46 +32,100 @@ class Router:
     def __init__(self, router_id):
         self.router_id = router_id
         self.links = []
-        self.f_table = {router_id: (0, 0)}  # forwarding table
+        self.f_table = {self.router_id: (0, 0)}  # forwarding table
         self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket to send
+        self.active = True
 
     def __str__(self):
+        """Prints out the forwarding table """
         table_format = "=" * 18
         table_format += " RIPv2 Routing Table of " + str(self.router_id) + " "
         table_format += "=" * 18 + "\n"
-        table_format += "Router Inputs: " + str(self.links) + "\n"  # we need to print forwarding table not links
+        table_format += "Router Inputs: " + str(self.links) + "\n"
+        table_format += f"{'Router Id':<15}{'Port':>6}{'Cost':>20}"
+        table_format += "\n" + "=" * 62 + "\n"
+        for key, value in sorted(self.f_table.items()):
+            port, cost = value
+            table_format += "{:<12} {:>6}  {:>20} \n".format(key, port, cost)
         print(table_format)
-
-    def __repr__(self):
-        return self.__str__()
 
     def add_link(self, link):
         self.links.append(link)  # link = (input, output, cost)
 
+    def toggle_activity(self):
+        """Turns router on/off and resets the f_table"""
+        self.active = not self.active
+        self.f_table = {self.router_id: (0, 0)}
+
     def recv_msg(self, msg, port):
-        msg_dst = msg[0:8]
-        print('Data received: ', msg, port, self.router_id)
-        if msg_dst == self.router_id:
-            print('Message received at router', self.router_id)
-        else:
-            if self.f_table.__contains__(msg_dst):
-                self.sender.sendto(msg, self.f_table[msg_dst][0])
+        if self.active:  # if router is active, process message as normal
+            self.process_msg(msg, port)
+        else:  # if router is not active check to see if packet type is activity toggle and turn back on if so.
+            if int(msg[6:8], 2) == 3:
+                self.toggle_activity()
+
+    def process_msg(self, msg, port):
+        try:
+            if str(int(msg[:6], 2)) == self.router_id or str(int(msg[:6], 2)) == '0':
+                msg_dst, typ, body = self.pkt_unravel(msg)
+                if typ == 0:
+                    print('Update f_table with', body)
+                    self.update_f_table(body, self.get_link(port))
+                if typ == 1:
+                    print('Router', self.router_id, 'received message', body)
+                if typ == 2:
+                    print(self.f_table)
+                if typ == 3:
+                    self.toggle_activity()
+            else:
+                msg_dst = str(int(msg[:6], 2))
+                print('Router', self.router_id, 'received message to forward to router', msg_dst)
+                if self.f_table.__contains__(msg_dst):
+                    target = 'localhost', self.f_table[msg_dst][0]
+                    self.sender.sendto(msg.encode('utf8'), target)
+        except ValueError:
+            print('Incorrect message format received at Router ID:', self.router_id)
+
+    def get_link(self, port):
+        for link in self.links:
+            if link[0] == port:
+                return link
+        print('[ERROR]: Link not found')
 
     def update_f_table(self, new_info, link):
-        """todo: write me"""
+        # check if forwarding table has the dictionary in it -  link from link function and new info
         # check if ID in dictionary, if not add to dictionary with port and cost from link + cost listed
         # if in dictionary see if cost is lower and update appropriately
 
+        base_cost = link[2]  # Get the cost of the link.
+        for dest in new_info:
+            if dest in self.f_table:  # See if we have info on the router.
+                current_best = self.f_table[dest][1]
+                new_potential_cost = new_info[dest][1] + base_cost
+                if current_best > new_potential_cost:  # If this is a better route we change our table entries.
+                    self.f_table[dest] = (link[1], new_potential_cost)
+            else:
+                cost = new_info[dest][1] + base_cost
+                self.f_table[dest] = (link[1], cost)  # We didn't have a route to here. So we just take any route info.
 
-# router_info ={'1': [[1112, 1116, 1117], [2221, 7777, 6666]],
-#               '2': [[2221, 2223], [1112, 2223]],
-#               '3': [[3332, 3334], [2223, 4443]],
-#               '4': [[4443, 4445, 4447], [7774, 3334, 5554]],
-#               '5': [[5554, 5556], [4445, 6665]],
-#               '6':[[6661, 6665], [1116, 5556]],
-#               '7': [[7771, 7774], [1117, 4447]]}
+    @staticmethod
+    def pkt_build(dst, typ, body):
+        """Creates the correctly formatted packet to send between routers"""
+        bin_dst = bin(int(dst))[2:].zfill(6)  # first 6 digits represent destin
+        bin_type = bin(int(typ))[2:].zfill(2)  # 2 digits for type = 4 possible types
+        bin_body = bin(int((json.dumps(body).encode('utf8')).hex(), 16))[2:]
+        return bin_dst + bin_type + bin_body
+
+    @staticmethod
+    def pkt_unravel(msg):
+        dst = str(int(msg[:6], 2))
+        typ = int(msg[6:8], 2)
+        body = json.loads(bytes.fromhex(hex(int(msg[8:], 2))[2:]))
+        return dst, typ, body
+
 
 test_routers = {'1': [(5000, 5001, 8), (5011, 41241, 8)]}  # dictionary format {id: [(input, output, cost), (link2))]}
+
 
 def main():
     """I run the show around here!"""
@@ -79,21 +133,20 @@ def main():
     routers = []  # create a list
     i = 0
 
+    # Config reader to take the dictionary of the router file
     router_file = sys.argv[1]
     router_id, inputs, outputs = config.read_router_file(router_file)
     config_file = config.Main(router_id, inputs, outputs)
     router_parse = config_file.parse_routing_dictionary(router_file)
 
-
-    # for the router in the dictionary
     for router in router_parse:
         routers.append(Router(router))
         route = Router(router)
         for link in router_parse[router]:
             servers.append(Server('localhost', int(link[0]), routers[i]))
             routers[i].add_link(link)
+        route.__str__()
         i += 1
-        #route.__str__()
 
     while True:
         readers, _, _ = select.select(servers, [], [])  # select takes 3 lists as input we only need first one
@@ -102,4 +155,3 @@ def main():
 
 
 main()
-
--- old_file.py
+++ old_file.py
@@ -1,3 +1,4 @@
+
 """A basic router for RIP implementation
 todo: Router: define f_table(forwarding table), write __str__ to print f_table, write recv_msg
 todo: Main: implement config reader and use to create server and router objects"""
@@ -7,119 +8,105 @@ import socket
 import select
 import threading  # Probably won't be used
 import sys
-
-# import config file
 import config
 
-# https://pythontic.com/modules/socket/udp-client-server-example
-"""
-Class: Server
-Create a UDP socket.
-Bind the socket to the server address.
-Wait until the datagram packet arrives from the client.
-Process the datagram packet and send a reply to the client.
-Go back to Step 3.
-"""
+
 class Server:
-    # Initialize udp socket and grab the dictionary file
-    def __init__(self, owner, address, port):
-        # Create a udp socket
+    def __init__(self, address, port, owner):
         self.receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket to receive
-        # Bind the socket to the server address
-        #self.receiver.bind(('localhost', port))
-
-        """
-        Attributes:
-        Address: Input port of Sending router
-        Port: Neighbor port it will send to
-        Owner: Neighbor Id
-        """
-        self.address = address
-        self.owner = owner
-        self.port = port #[[1112, 1, 1]]
-        #for ports in self.port:
-        #   for output, id, cost in self.port:
-        #       neighbor_port, neighbor_cost, neighbor_id = output, cost, id
-        self.hop = 0
-        self.next_hop = 1
-
+        host = 'localhost', port
+        self.port = port
+        self.receiver.bind(host)
+        self.owner = owner  # the router which owns this port
 
     def fileno(self):  # required for select
         return self.receiver.fileno()
 
     def on_read(self):  # the method for receiving a message
         message = self.receiver.recv(1024).decode('utf-8')
-        print('Send to ', self.owner)
+        #print('Send to ', self.owner)
         self.owner.recv_msg(message, self.port)
 
 
-"""
-Class: Router
-Create a UDP socket.
-Send a message to the server.
-Wait until response from the server is received.
-Process reply and go back to step 2, if necessary.
-Close socket descriptor and exit.
-"""
-class Router():
-    """ will initialize the configuration class. The paramaters are used since it is required"""
-    # initialize the router
-    def __init__(self, parsed_file):
-        #self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Socket to send
-        self.links = []
-        self.forwarding_table = {}
-        # router dictionary from config.py
-        self.parsed_file = parsed_file
-        routing_dictionary = self.parsed_file
-        # grabbing the router_id of the router
-        for key in routing_dictionary.keys():
-            router_id = key
+class Router:
+    def __init__(self, router_id):
         self.router_id = router_id
+        self.links = []
+        self.forwarding_table = {router_id: (0, 0)}  # forwarding table port, cost
+        self.sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket to send
 
-        input_ports = []
-        for value in routing_dictionary.items():
-            value = value[1]
-            for input, port, output in value:
-                input_port = input
-                input_ports.append(input)
-        self.input_ports = input_port
-
-
-
-    def add_link(self, link):
-        self.links.append(link)
-
-    # Display the information of the routing dictionary
     def __str__(self):
-        table_format = "=" * 18
+        """Prints out the forwarding table """
+        table_format = "="*18
         table_format += " RIPv2 Routing Table of " + str(self.router_id) + " "
-        table_format += "=" * 18 + "\n"
-        table_format += "Router Inputs: " + str(self.input_ports) + "\n"
+        table_format += "="*18 + "\n"
+        table_format += "Router Inputs: " + str(self.links) + "\n"
         table_format += f"{'Router Id':<15}{'Port':>6}{'Cost':>20}{'Next Hop':>21}"
-        table_format += "\n" + "=" * 62 + "\n"
-        """
-        cost = Server.cost
-        for key, value in sorted(self.parsed_file):
-            value = value[1]
-            for input, port, output in value:
-                table_format += "{:<12} {:>6}  {:>20} \n".format(key, port, , cost)
-        """
+        table_format += "\n" + "="*62 + "\n"
+        for key, value in sorted(self.forwarding_table.items()):
+            port, cost = value
+            table_format += "{:<12} {:>6}  {:>20} \n".format(key, port, cost)
         print(table_format)
 
-    def __repr(self):
-       return self.__str__()
+    def add_link(self, link):
+        links = self.links
+        links.append(link)  # link = (input, output, cost)
+        #print(links) [[1112, 1, 2221], [1117, 8, 7771], [1116, 5, 6661]]
+
+    def update_forwarding_table(self):
+        """todo: write me"""
+        # check if forwarding table has the dictionary in it -  link from link function and new info
+        # check if ID in dictionary, if not add to dictionary with port and cost from link + cost listed
+        # if in dictionary see if cost is lower and update appropriately
+
+        for link in self.links:
+            print(link)
+        # for router, contents in self.forwarding_table.items():
+        #    router = self.router_id
+        #   contents = self.links
+        # for router, contents in self.forwarding_table.items():
+        # if router == self.router_id:
+        # continue)
+        # elif router not in self.forwarding_table[1].keys():
+        #    self.forwarding_table[1][router] = [contents[0], contents[1], link+contents[2]]
 
     def recv_msg(self, msg, port):
-        msg_dst = msg[0:8]
-        print('Data received: ', msg, port, self.router_id)
-        if msg_dst == self.router_id:
-            print('Message received at router', self.router_id)
-        else:
-            if self.f_table.__contains__(msg_dst):
-                self.sender.sendto(msg, self.f_table[msg_dst][0])
+        try:
+            if str(int(msg[:6], 2)) == self.router_id or str(int(msg[:6], 2)) == '0':
+                print(str(int(msg[:6], 2)))
+                msg_dst, typ, body = self.pkt_unravel(msg)
+                if typ == 0:
+                    print('Update f_table with', body)
+                if typ == 1:
+                    print('Router', self.router_id, 'received message', body)
+                if typ == 2:
+                    # print the f_table
+                    pass
+                if typ == 3:
+                    # maybe disable router?
+                    pass
+            else:
+                msg_dst = str(int(msg[:6], 2))
+                print('Router', self.router_id, 'received message to forward to router', msg_dst)
+                if self.f_table.__contains__(msg_dst):
+                    self.sender.sendto(msg, self.f_table[msg_dst][0])
+        except ValueError:
+            print('Incorrect message format received at Router ID:', self.router_id)
 
-    def update_forwarding_table(self, new_info):
-        """todo: write me"""
+    @staticmethod
+    def pkt_build(dst, typ, body):
+        """Creates the correctly formatted packet to send between routers"""
+        bin_dst = bin(int(dst))[2:].zfill(6)  # first 6 digits represent destin
+        bin_type = bin(int(typ))[2:].zfill(2)  # 2 digits for type = 4 possible types
+        bin_body = bin(int((json.dumps(body).encode('utf8')).hex(), 16))[2:]
+        return bin_dst + bin_type + bin_body
+
+    @staticmethod
+    def pkt_unravel(msg):
+        dst = str(int(msg[:6], 2))
+        typ = int(msg[6:8], 2)
+        body = json.loads(bytes.fromhex(hex(int(msg[8:], 2))[2:]))
+        return dst, typ, body
 
 
 # router_info ={'1': [[1112, 1116, 1117], [2221, 7777, 6666]],
@@ -130,41 +117,38 @@ class Router():
 #               '6':[[6661, 6665], [1116, 5556]],
 #               '7': [[7771, 7774], [1117, 4447]]}
 
-test_routers = {'1': [(5000, 5001, 8)],
-               '2': [(5001, 5000, 8)]}  # dictionary format {id: [(input, output, cost], (link2))]}
+#test_routers = {'1': [(5000, 5001, 8), (5011, 41241, 8)]}  # dictionary format {id: [(input, output, cost), (link2))]}
+
 
 def main():
     """I run the show around here!"""
-    try:
-        if len(sys.argv) < 2:
-            sys.exit()
-
-        router_file = sys.argv[1]
-        router_id, inputs, outputs = config.read_router_file(router_file)
-        config_file = config.Main(router_id, inputs, outputs)
-        router_parse = config_file.parse_routing_dictionary(router_file)
-
-        if not config_file:
-            sys.exit("Invalid configuration file")
-        print("Configuration has been loaded!")
-        # Read the router ID from grabbing it from Main
-        router = Router(router_parse)
-        print("Start router:", str(router_id))
-        server = Server(router_id, inputs, outputs)
-
-    except IndexError:
-        sys.exit("Argument is invalid!")
-
-    '''
-    servers = []
-    routers = []
+    servers = []  # create a list to hold servers
+    routers = []  # create a list whose routes in the forwarding table
     i = 0
-    for router in test_routers:
+
+    # Config reader to take the dictionary of the router file
+    router_file = sys.argv[1]
+    router_id, inputs, outputs = config.read_router_file(router_file)
+    config_file = config.Main(router_id, inputs, outputs)
+    router_parse = config_file.parse_routing_dictionary(router_file)
+
+    for router in router_parse:
         routers.append(Router(router))
-        for link in test_routers[router]:
+        route = Router(router)
+        for link in router_parse[router]:
             servers.append(Server('localhost', int(link[0]), routers[i]))
             routers[i].add_link(link)
+        route.__str__()
         i += 1
-    '''
+
+
+
+
+    while True:
+        readers, _, _ = select.select(servers, [], [])  # select takes 3 lists as input we only need first one
+        for reader in readers:
+            reader.on_read()
+
 
 main()
+
