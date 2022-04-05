9232aad24bad6c4f52d459a46966abf4a5411056
Lenovo <ccq10@uclive.ac.nz>
2022-04-06 09:52:12 +1200

    added the routerv1 functions into router v0

--- Router_v0.1.py
+++ Router_v0.1.py
@@ -6,11 +6,22 @@ todo: Main: implement config reader and use to create server and router objects"
 
 import socket
 import select
-import threading  # Probably won't be used
+import threading
 import sys
 import json
 import config
 
+class Ticker:
+    """Ticks every duration"""
+    def __init__(self, duration, owner):
+        self.thread = None
+        self.duration = duration
+        self.owner = owner
+
+    def loop(self):
+        self.owner.on_tick()  # the true action
+        self.thread = threading.Timer(self.duration, self.loop)  # sets a timer with function to call self.on_tick
+        self.thread.start()  # starts that timer
 
 class Server:
     def __init__(self, address, port, owner):
@@ -26,7 +37,6 @@ class Server:
     def on_read(self):  # the method for receiving a message
         message = self.receiver.recv(1024).decode('utf-8')
         self.owner.recv_msg(message, self.port)
-
 
 class Router:
     def __init__(self, router_id):
@@ -53,20 +63,30 @@ class Router:
         return table_format
 
     def add_link(self, link):
-        self.links.append(link)  # link = (input, output, cost)
+        self.links.append(link)  # link = (input, output, cost, time since update)
 
     def toggle_activity(self):
         """Turns router on/off and resets the f_table"""
         self.active = not self.active
         self.f_table = {self.router_id: (0, 0)}
-
     # check if active and then process_msg
+
+
     def recv_msg(self, msg, port):
+        """If we are active send the message to process message if not check for activity toggle packet"""
         if self.active:  # if router is active, process message as normal
             self.process_msg(msg, port)
         else:  # if router is not active check to see if packet type is activity toggle and turn back on if so.
             if int(msg[6:8], 2) == 3:
                 self.toggle_activity()
+
+    def broadcast(self):
+        """Sends out the forwarding information we have to our neighbors"""
+        for link in self.links:
+            target = self.network_id, link[1]
+            msg = self.pkt_build(0, 0, self.f_table)
+            self.sender.sendto(msg.encode('utf8'), target)
+
 
     def process_msg(self, msg, port):
         try:
@@ -101,24 +121,16 @@ class Router:
         print('[ERROR]: Link not found')
 
     def update_f_table(self, new_info, link):
-        # add cost
-        max_cost = 16
+        link[3] = 0  # update ticks since last message
         base_cost = link[2]  # Get the cost of the link.
-        # dest = destination key
         for dest in new_info:
             if dest in self.f_table:  # See if we have info on the router.
                 current_best = self.f_table[dest][1]
                 new_potential_cost = new_info[dest][1] + base_cost
-                if current_best < 16:
-                    if current_best > new_potential_cost:  # If this is a better route we change our table entries.
-                        self.f_table[dest] = (link[1], new_potential_cost)
-                else:
-                    current_best = 16
-                    self.f_table[dest] = (link[1], current_best)
+                if current_best > new_potential_cost:  # If this is a better route we change our table entries.
+                    self.f_table[dest] = (link[1], new_potential_cost)  # output port, new cost
             else:
                 cost = new_info[dest][1] + base_cost
-                if cost >= 16:
-                    self.f_table[dest] = (link[1], 16)
                 self.f_table[dest] = (link[1], cost)  # We didn't have a route to here. So we just take any route info.
 
     @staticmethod
@@ -136,6 +148,35 @@ class Router:
         body = json.loads(bytes.fromhex(hex(int(msg[8:], 2))[2:]))
         return dst, typ, body
 
+    def on_tick(self):
+        """responsible for periodic updates"""
+        self.broadcast()
+        for link in self.links:
+            link[3] += 1
+            if link[3] >= 6:
+                self.kill_link(link)
+
+    def kill_link(self, link):
+        for dest in self.f_table:
+            if self.f_table[dest][0] == link[1]:
+                self.f_table[dest] = (link[1], 16)
+
+
+def setup():
+    servers = []  # create a list to hold servers
+    routers = []  # create a list
+    tickers = []
+    i = 0
+    network_id = 'localhost'
+    for router in test_routers:
+        routers.append(Router(router, network_id))
+        for link in test_routers[router]:
+            link.append(0)
+            servers.append(Server(network_id, int(link[0]), routers[i]))
+            routers[i].add_link(link)
+        tickers.append(Ticker(3, routers[i]))
+        i += 1
+    return servers, tickers
 
 def main():
     """I run the show around here!"""
